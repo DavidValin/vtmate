e59b8067973568a87c37f534c4754ee084341b5a
David Valin <hola@davidvalin.com>
2026-09-12 22:37:45 +0200

    add extra necessary info

--- src/daemon/mod.rs
+++ src/daemon/mod.rs
@@ -215,7 +215,7 @@ fn print_shortcut_help(d: &config::DaemonSettings) {
   );
   println!("    What: Ask the selected agent via voice and listen the reply");
   println!(
-    "    e.g. ai voice conversation, find information, ask about a specific text selection, ..."
+    "    e.g. ai voice conversation, find information, ask about a text selection, ..."
   );
   println!();
 
@@ -224,7 +224,8 @@ fn print_shortcut_help(d: &config::DaemonSettings) {
   println!("    What: stops ongoing playback");
   println!();
   how_line("Press ", &d.llm_background_reset, " twice");
-  println!("    What: resets the llm conversation");
+  println!("    What: resets the llm conversation (and stops saving if session is being saved)");
+
   println!();
 
   println!("  tts_background_combo:");
